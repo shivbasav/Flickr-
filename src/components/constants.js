3f5a04e5d38526ddc6a7ea00e5b1f099573e0b2a
Shivbasav Nandargi <shivagn119@gmail.com>
2019-02-02 12:38:22 +0530

    First

---
+++ src/components/constants.js
@@ -0,0 +1,7 @@
+const constants = {
+	BASE_URL:
+		"https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=766273b23c84db8ea559f86ffecd1b1d&per_page=20",
+	STORAGE_KEY: "flicker_search_queries"
+};
+
+export default constants;
